a67b71f9e852e50802cd0cd2e65959521088c3eb
Kevin David Cuadros <63522369+kevxo@users.noreply.github.com>
2026-06-07 17:27:58 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 9
+print(type(variable))
+variable = 9.9
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Nine"
+print(type(variable))
+variable = [9]
+print(type(variable))
